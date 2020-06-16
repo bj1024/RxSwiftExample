37c9893407818cefd2d08b501d12277f9bfb3dd9
H.K <16552133+bj1024@users.noreply.github.com>
2020-06-16 20:19:26 +0900

    use bind value to label.

--- RxSwiftExample/Counter/CounterViewModel.swift
+++ RxSwiftExample/Counter/CounterViewModel.swift
@@ -9,8 +9,19 @@ import RxSwift
 
 protocol CounterViewModelProtocol {
   var count:BehaviorRelay<Int> {get}
+  func countUp()
+  func countDown()
 }
 
 class CounterViewModel:CounterViewModelProtocol{
- var count = BehaviorRelay<Int>(value: 0)
+  var count = BehaviorRelay<Int>(value: 0)
+
+  func countUp(){
+    count.accept(count.value + 1)
+  }
+
+  func countDown(){
+    count.accept(count.value - 1)
+  }
+
 }
