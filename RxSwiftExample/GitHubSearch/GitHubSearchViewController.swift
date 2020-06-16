f9eff60e11c48bb02751157038d0ec8c3b3836f1
H.K <16552133+bj1024@users.noreply.github.com>
2020-06-16 12:01:35 +0900

    add SearchController and remove ScheDelegate for iOS12

--- RxSwiftExample/GitHubSearch/GitHubSearchViewController.swift
+++ RxSwiftExample/GitHubSearch/GitHubSearchViewController.swift
@@ -14,15 +14,22 @@ class GitHubSearchViewController: UIViewController {
   private var viewModel:GitHubSearchViewModelProtocol = GitHubSearchViewModel()
   private let disposeBag = DisposeBag()
 
+  @IBOutlet var tableView: UITableView!
+  let searchController = UISearchController(searchResultsController: nil)
 
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    tableView.scrollIndicatorInsets.top = tableView.contentInset.top
+    searchController.obscuresBackgroundDuringPresentation = false
+    navigationItem.searchController = searchController
+
     // Do any additional setup after loading the view.
     bind()
   }
 
+  
   func setViewModel(viewModel:GitHubSearchViewModelProtocol){
     self.viewModel = viewModel
   }
@@ -30,4 +37,11 @@ class GitHubSearchViewController: UIViewController {
   func bind(){
   }
 
+  override func viewDidAppear(_ animated: Bool) {
+     super.viewDidAppear(animated)
+     UIView.setAnimationsEnabled(false)
+     searchController.isActive = true
+     searchController.isActive = false
+     UIView.setAnimationsEnabled(true)
+   }
 }
