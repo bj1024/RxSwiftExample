4ebdbf9c9113fe2bf5fcfe744fe829a97d85df55
H.K <16552133+bj1024@users.noreply.github.com>
2020-06-17 20:41:59 +0900

    Rx rest api call.

--- RxSwiftExample/GitHubSearch/GitHubSearchViewController.swift
+++ RxSwiftExample/GitHubSearch/GitHubSearchViewController.swift
@@ -15,18 +15,29 @@ class GitHubSearchViewController: UIViewController {
   private let disposeBag = DisposeBag()
 
   @IBOutlet var tableView: UITableView!
+
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
   let searchController = UISearchController(searchResultsController: nil)
 
 
   override func viewDidLoad() {
     super.viewDidLoad()
+     print("viewDidLoad GitHubSearchViewController")
+//    tableView.delegate = self
+//       tableView.dataSource = self
 
     tableView.scrollIndicatorInsets.top = tableView.contentInset.top
     searchController.obscuresBackgroundDuringPresentation = false
+    searchController.searchBar.autocapitalizationType = .none
     navigationItem.searchController = searchController
 
     // Do any additional setup after loading the view.
     bind()
+  }
+
+  deinit {
+    print("deinit GitHubSearchViewController")
   }
 
   
@@ -35,6 +46,52 @@ class GitHubSearchViewController: UIViewController {
   }
 
   func bind(){
+    // Action
+
+    // orEmpty
+    let searchTextObservable = searchController.searchBar.rx.text.orEmpty.asObservable()
+
+    searchTextObservable
+      .debounce(.milliseconds(500), scheduler: MainScheduler.instance)
+      .subscribe(onNext: { [unowned self] event in
+          print("tap event: \(event)")
+        self.viewModel.setKeyword(kw:event)
+      })
+
+    .disposed(by: disposeBag)
+//
+//    viewModel.keyword
+//    .bind(to: tableView.rx.items(cellIdentifier: "cell")) { indexPath, repo, cell in
+//      cell.textLabel?.text = repo
+//    }
+//    .disposed(by: disposeBag)
+
+    viewModel.results
+         .bind(to: tableView.rx.items(cellIdentifier: "cell")) { indexPath, repo, cell in
+           cell.textLabel?.text = repo
+         }
+         .disposed(by: disposeBag)
+
+    viewModel.isProcessing
+      .observeOn(MainScheduler.instance)
+    .subscribe(onNext: { [unowned self] val in
+      self.activityIndicator.isHidden = !val
+      if val {
+        self.activityIndicator.startAnimating()
+      }
+      else{
+        self.activityIndicator.stopAnimating()
+      }
+     })
+     .disposed(by: disposeBag)
+
+//    viewModel.isProcessing.map{ !$0 }
+//      .bind(to: activityIndicator.rx.isHidden)
+//    .disposed(by: disposeBag)
+//    viewModel.isProcessing.map{ $0 }
+//      .bind(to: activityIndicator.rx.isAnimating)
+//    .disposed(by: disposeBag)
+
   }
 
   override func viewDidAppear(_ animated: Bool) {
@@ -45,3 +102,37 @@ class GitHubSearchViewController: UIViewController {
      UIView.setAnimationsEnabled(true)
    }
 }
+
+//
+//extension GitHubSearchViewController:UITableViewDelegate{
+//
+//}
+//
+//extension GitHubSearchViewController:UITableViewDataSource{
+//
+//  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//    return 0
+//  }
+//
+//  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//    let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+//
+//    let menuIdx = indexPath.row
+//    cell.accessoryType = .disclosureIndicator
+//    cell.textLabel?.text = "\(indexPath.row + 1)"
+//    return cell
+//  }
+//  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//
+//  }
+//
+//  func numberOfSections(in tableView: UITableView) -> Int {
+//    return 1
+//  }
+//
+//
+//}
+
+
--- RxSwiftExample/GitHubSearch/GitHubSearchViewModel.swift
+++ RxSwiftExample/GitHubSearch/GitHubSearchViewModel.swift
@@ -5,9 +5,99 @@ import RxSwift
 
 
 protocol GitHubSearchViewModelProtocol {
-  var count:BehaviorRelay<Int> {get}
+  var keyword:BehaviorRelay<String> {get}
+  var results:BehaviorRelay<[String]> {get}
+  var isProcessing:BehaviorRelay<Bool> {get}
+  func setKeyword(kw:String)
 }
 
 class GitHubSearchViewModel:GitHubSearchViewModelProtocol{
- var count = BehaviorRelay<Int>(value: 0)
+  var keyword  =  BehaviorRelay<String>(value: "")
+
+  var results = BehaviorRelay<[String]>(value: ["a","b","c"])
+  var isProcessing  =  BehaviorRelay<Bool>(value:false)
+
+
+  private let disposeBag = DisposeBag()
+
+
+  func setKeyword(kw:String){
+    keyword.accept(kw)
+
+
+    print("GitHubSearchViewModel isProcessing = \(self.isProcessing.value)")
+    isProcessing.accept(true)
+
+    let observableResult = search(keyword: kw)
+    .subscribe(onNext: { [unowned self] repos in
+//        print(repos)
+      self.isProcessing.accept(false)
+      self.results.accept(repos)
+    })
+     .disposed(by: disposeBag)
+//
+//    let cancelRequest = responseJSON
+//        // this will fire the request
+//        .subscribe(onNext: { json in
+//            print(json)
+//        })
+
+//    Thread.sleep(forTimeInterval: 3.0)
+
+    // if you want to cancel request after 3 seconds have passed just call
+//    observableResult.dispose()
+  //    DispatchQueue.global(qos: .default).asyncAfter(deadline: .now() + .seconds(3)){
+  //      DispatchQueue.main.async {
+  //          self.isProcessing.accept(false)
+  //        print("GitHubSearchViewModel isProcessing = \(self.isProcessing.value)")
+  //      }
+  //    }
+
+
+}
+
+private func search(keyword:String) -> Observable<[String]>{
+
+  guard let url =  URL(string: "https://api.github.com/search/repositories?q=\(keyword)") else {return .just([])}
+  let observableResult:Observable<[String]> = URLSession.shared.rx.json(url: url)
+    .map { json -> ([String]) in
+      guard let dict = json as? [String: Any] else { return [] }
+      guard let items = dict["items"] as? [[String: Any]] else { return [] }
+      let repos = items.compactMap { $0["full_name"] as? String }
+      //              let nextPage = repos.isEmpty ? nil : page + 1
+
+      return repos
+  }
+
+  .do(onError: { error in
+    if case let .some(.httpRequestFailed(response, _)) = error as? RxCocoaURLError, response.statusCode == 403 {
+      print("⚠️ GitHub API rate limit exceeded. Wait for 60 seconds and try again.")
+    }
+  })
+    .catchErrorJustReturn([])
+
+  return observableResult
+
+
+}
+
+//   private func search(query: String?, page: Int) -> Observable<(repos: [String], nextPage: Int?)> {
+//      let emptyResult: ([String], Int?) = ([], nil)
+//      guard let url = self.url(for: query, page: page) else { return .just(emptyResult) }
+//      return URLSession.shared.rx.json(url: url)
+//        .map { json -> ([String], Int?) in
+//          guard let dict = json as? [String: Any] else { return emptyResult }
+//          guard let items = dict["items"] as? [[String: Any]] else { return emptyResult }
+//          let repos = items.compactMap { $0["full_name"] as? String }
+//          let nextPage = repos.isEmpty ? nil : page + 1
+//          return (repos, nextPage)
+//        }
+//        .do(onError: { error in
+//          if case let .some(.httpRequestFailed(response, _)) = error as? RxCocoaURLError, response.statusCode == 403 {
+//            print("⚠️ GitHub API rate limit exceeded. Wait for 60 seconds and try again.")
+//          }
+//        })
+//        .catchErrorJustReturn(emptyResult)
+//    }
+//  }
 }
