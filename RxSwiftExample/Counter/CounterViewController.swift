5f353709e15e213f26d5b467bff36118016b60fe
H.K <16552133+bj1024@users.noreply.github.com>
2020-06-18 21:27:04 +0900

    Format Code

--- RxSwiftExample/Counter/CounterViewController.swift
+++ RxSwiftExample/Counter/CounterViewController.swift
@@ -1,21 +1,16 @@
 
-import UIKit
 import RxCocoa
 import RxSwift
-
+import UIKit
 
 class CounterViewController: UIViewController {
-
   @IBOutlet weak var countLabel: UILabel!
   @IBOutlet weak var btnPlus: UIButton!
   @IBOutlet weak var btnMinus: UIButton!
 
-
-  private var viewModel:CounterViewModelProtocol = CounterViewModel()
+  private var viewModel: CounterViewModelProtocol = CounterViewModel()
 
   private let disposeBag = DisposeBag()
-
-
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -25,33 +20,31 @@ class CounterViewController: UIViewController {
     //    setViewModel(viewModel: CounterViewModel())
     bind()
   }
+
   deinit {
     print("deinit CounterViewController")
   }
 
-  func setViewModel(viewModel:CounterViewModelProtocol){
+  func setViewModel(viewModel: CounterViewModelProtocol) {
     self.viewModel = viewModel
   }
 
-
-  func bind(){
+  func bind() {
     btnPlus.rx.tap
-      .subscribe(onNext: {[unowned self] _ in
-       self.viewModel.countUp()
+      .subscribe(onNext: { [unowned self] _ in
+        self.viewModel.countUp()
       })
       .disposed(by: disposeBag)
 
     btnMinus.rx.tap
-      .subscribe(onNext: {[unowned self] _ in
+      .subscribe(onNext: { [unowned self] _ in
         self.viewModel.countDown()
 
       })
       .disposed(by: disposeBag)
 
-    viewModel.count.map{"\($0)"}
-      .bind(to:self.countLabel.rx.text)
+    viewModel.count.map { "\($0)" }
+      .bind(to: countLabel.rx.text)
       .disposed(by: disposeBag)
-
-
   }
 }
