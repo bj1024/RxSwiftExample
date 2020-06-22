a7ab8b809bcb0e57d6680e701d2a20e0eefe7fc7
H.K <16552133+bj1024@users.noreply.github.com>
2020-06-22 17:45:29 +0900

    Mobe ViewModelType to common.

--- RxSwiftExample/Counter/CounterViewModel.swift
+++ RxSwiftExample/Counter/CounterViewModel.swift
@@ -7,30 +7,8 @@ import RxSwift
 // RxSwift + MVVM Example
 //
 
-
-
 //
 // RxSwift + MVVM: how to feed ViewModels - BlaBlaCar Tech - Medium https://medium.com/blablacar-tech/rxswift-mvvm-66827b8b3f10
-
-// ViewModelの形を定義するprotocol。
-// ViewModelは次を保持する。（わかりやすいように、入出力、外部依存別に分ける。）
-//
-//  Input:ViewController → ViewModel
-//  Output:ViewModel → ViewController
-//  Dependancy: ViewModelが依存する処理（REST APICALLなど）
-//
-//  Input Output Dependancyの詳細な型はわからないので、associatedtypeとし、
-//  各ViewModel実装時に任せる。
-//  このprotocolでは Input Output Dependancyが必要ですよと宣言するのみ。
-//
-protocol ViewModelType {
-  associatedtype Input
-  associatedtype Output
-  associatedtype Dependancy
-  var input: Input { get }
-  var output: Output { get }
-  var dependancy: Dependancy{ get }
-}
 
 // CounterViewModelでの カウント処理のprotocol
 // MVVM参考実装として、カウント処理を外部依存として実装してみる。
