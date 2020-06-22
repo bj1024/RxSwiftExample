7394ceadabf7e3d3bffbdbaf94961ae7411067be
H.K <16552133+bj1024@users.noreply.github.com>
2020-06-22 17:45:29 +0900

    Mobe ViewModelType to common.

---
+++ RxSwiftExample/Common/ViewModelType.swift
@@ -0,0 +1,24 @@
+
+//
+// RxSwift + MVVM Example
+//
+
+// ViewModelの形を定義するprotocol。
+// ViewModelは次を保持する。（わかりやすいように、入出力、外部依存別に分ける。）
+//
+//  Input:ViewController → ViewModel
+//  Output:ViewModel → ViewController
+//  Dependancy: ViewModelが依存する処理（REST APICALLなど）
+//
+//  Input Output Dependancyの詳細な型はわからないので、associatedtypeとし、
+//  各ViewModel実装時に任せる。
+//  このprotocolでは Input Output Dependancyが必要ですよと宣言するのみ。
+//
+protocol ViewModelType {
+  associatedtype Input
+  associatedtype Output
+  associatedtype Dependancy
+  var input: Input { get }
+  var output: Output { get }
+  var dependancy: Dependancy{ get }
+}
